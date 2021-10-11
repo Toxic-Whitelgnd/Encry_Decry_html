2ecc269cd277983baec4a7deeecf6d037b3edbca
Toxic-Whitelgnd <90614827+Toxic-Whitelgnd@users.noreply.github.com>
2021-10-11 14:33:36 +0530

    Add files via upload

---
+++ Content_page.js
@@ -0,0 +1,9 @@
+function encryption(){
+    document.location.href="Encryption_page.html";
+    alert("this is secure page!")
+}
+
+
+function decryption(){
+    document.location.href="Decryption_page.html"
+}
---
+++ Login_js.js
@@ -0,0 +1,10 @@
+function login(){
+    document.location.href="Context.html";
+}
+
+
+
+{/* <script src="https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js"></script> */}
+//    <script src="https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js"></script>
+//    <script src="https://www.gstatic.com/firebasejs/9.1.2/firestore.js"></script>
+    
